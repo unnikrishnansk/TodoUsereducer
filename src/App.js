e1d3fffa0a1a8adf537334f99b902188d6f747f2
unnikrishnansk <unni.sk94@gmail.com>
2022-06-17 20:48:26 +0530

    commit todo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
+
+import { useReducer , useState } from 'react';
 import './App.css';
 
+
+const reducer = (state,action) => {
+
+  switch(action.type){
+    case "add-todo":
+      return {
+        todos : [...state.todos,{text:action.text,completed:false}]
+      }
+      case "toggle-todo":
+        return {
+          todos : state.todos.map((todo,index) => index === action.index ? {...todo,completed:!todo.completed}:todo)
+        }
+        case "delete-todo":
+        return {
+          todos : state.todos.filter((todo,index) => index !== action.index)
+        }
+      default : 
+      return state;
+  }
+}
+
 function App() {
+
+  const [{todos,todocount} , dispatch] = useReducer(reducer,{
+    todos : [],
+    todocount : 0
+  });
+
+  const [text, settext] = useState("");
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+    <form onSubmit={e => {
+      e.preventDefault();
+      dispatch({type:'add-todo',text})
+      settext("");
+    }}>
+      <input type="text" value={text} onChange={e => settext(e.target.value)} />
+    </form>
+
+    {todos.map((todo,index) => (
+      <div key={index}
+      style={{textDecoration:todo.completed ? "line-through" : ""}}
+      >{todo.text}
+      <button  onClick={() => dispatch({type : 'toggle-todo',index})}>TOGGLE</button>
+      <button onClick={() => dispatch({type : 'delete-todo',index})}>DLT</button>
+      </div>
+    ))}
     </div>
   );
 }
